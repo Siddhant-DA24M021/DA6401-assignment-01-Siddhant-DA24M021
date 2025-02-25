d8693a4381686082f6e7420f6ae0c8ea5bb07ba3
Siddhant-DA24M021 <da24m021@smail.iitm.ac.in>
2025-02-25 15:07:49 +0530

    added forward and backward functionality

---
+++ loss.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+class CrossEntropyLoss:
+    def __init__(self):
+        self.preds = None
+        self.targets = None
+
+    def __call__(self, preds, targets):
+        self.preds = preds # batch_size x 10
+        self.targets = targets # batch_size x 10
+
+        # Targets are One-Hot Encoded
+        losses = -np.sum(targets * np.log(self.preds + 1e-15), axis = 1)
+        return np.mean(losses)
+    
+    def backward(self):
+        grad = - self.targets / (self.preds + 1e-15)
+        return grad # Derivative of loss function w.r.t. predictions (y-hat)
+    
+class SquaredErrorLoss:
+    def __init__(self):
+        self.preds = None
+        self.targets = None
+
+    def __call__(self, preds, targets):
+        self.preds = preds # batch_size x 10
+        self.targets = targets # batch_size x 10
+
+        # Targets are One-Hot Encoded
+        losses = np.sum((self.target - self.preds)**2, axis = 1)
+        return np.mean(losses)
+    
+    def backward(self):
+        grad = 2 * (self.target - self.preds)
+        return grad # Derivative of loss function w.r.t. predictions (y-hat)
