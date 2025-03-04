removed bug from loss gradient calculation

loss.py
@@ -5,17 +5,19 @@ class CrossEntropyLoss:
     def __init__(self):
         self.preds = None
         self.targets = None
+        self.batch_size = None
 
     def __call__(self, preds, targets):
         self.preds = preds # batch_size x num_class
         self.targets = targets # batch_size x num_class
+        self.batch_size = self.preds.shape[0]
 
         # Targets are One-Hot Encoded
         losses = -np.sum(targets * np.log(self.preds + 1e-15), axis = 1)
         return np.mean(losses)
     
     def backward(self):
-        grad = - self.targets / (self.preds + 1e-15)
+        grad = - self.targets / ((self.preds + 1e-15) * self.batch_size)
         return grad # Derivative of loss function w.r.t. predictions (y-hat)
 
 
@@ -23,15 +25,17 @@ class SquaredErrorLoss:
     def __init__(self):
         self.preds = None
         self.targets = None
+        self.batch_size = None
 
     def __call__(self, preds, targets):
         self.preds = preds # batch_size x num_class
         self.targets = targets # batch_size x num_class
+        self.batch_size = self.preds.shape[0]
 
         # Targets are One-Hot Encoded
         losses = np.sum((self.targets - self.preds)**2, axis = 1)
         return np.mean(losses)
     
     def backward(self):
-        grad = -2 * (self.targets - self.preds)
+        grad = -2 * (self.targets - self.preds) / self.batch_size
         return grad # Derivative of loss function w.r.t. predictions (y-hat)

optimizer.py
@@ -57,7 +57,7 @@ class RMSProp():
     def step(self, gradients):
         for i, (parameter, grad) in enumerate(zip(self.parameters, gradients)):
             self.u[i] = self.beta * self.u[i] + (1 - self.beta) * grad**2
-            effective_lr = self.learning_rate / (self.u[i] + self.epsilon)**0.5
+            effective_lr = self.learning_rate / np.sqrt(self.u[i] + self.epsilon)
             parameter -= effective_lr * grad
 
 # Adam